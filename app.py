c841c387a7eaa84dfb7d0d65aad5ef3282e543fd
rpradaa <Riya.Pradhan.US@gmail.com>
2025-07-09 20:39:13 -0400

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ CORS(app)
 
 @app.after_request
 def after_request(response):
-    response.headers['Access-Control-Allow-Origin'] = 'http://localhost:3000'
+    response.headers['Access-Control-Allow-Origin'] = '*'
     response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'
     response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
     response.headers['Access-Control-Allow-Credentials'] = 'true'

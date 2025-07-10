e2e11016ddd18e68e225a05ad7db7b6b00e8e1fc
rpradaa <Riya.Pradhan.US@gmail.com>
2025-07-09 20:02:33 -0400

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Portfolio from './Portfolio';
+import Portfolio from './Portfolio.tsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<Portfolio />);
